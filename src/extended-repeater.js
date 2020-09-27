716121f8a6ddffe347214a337299d7aec65c8df4
Evgene Popolamov <popolamov908@gmail.com>
2020-09-27 22:53:26 +0300

    feat: add solve extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,35 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  const res = [];
+
+  if (options.repeatTimes == undefined) {
+    res.push(str + "");
+    if (options.additionRepeatTimes == undefined) {
+      res.push(options.addition + "");
+    }
+  } else {
+    for (let i = 0; i < options.repeatTimes; i++) {
+      res.push(str + "");
+      for (let j = 0; j < options.additionRepeatTimes; j++) {
+        res.push(options.addition + "");
+        if (j != options.additionRepeatTimes - 1) {
+          if (options.additionSeparator == undefined) {
+            res.push("|");
+          } else {
+            res.push(options.additionSeparator + "");
+          }
+        }
+      }
+      if (i != options.repeatTimes - 1) {
+        if (options.separator == undefined) {
+          res.push("+");
+        } else {
+          res.push(options.separator + "");
+        }
+      }
+    }
+  }
+
+  return res.join("");
 };
-  
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
+module.exports = function transform(arr) {
+  throw new CustomError("Not implemented");
   // remove line with error and write your code here
+  // if (!Array.isArray(arr)) {
+  //   throw new Error("Error");
+  // }
+
+  // const newArr = [...arr];
+
+  // for (let i = 0; i <= newArr.length - 1; i++) {
+  //   if (newArr[i] === "--discard-next" && i != newArr.length - 1) {
+  //     newArr.splice(i, 2);
+  //   } else if (newArr[i] === "--discard-next" && i === newArr.length - 1) {
+  //     return arr;
+  //   } else if (newArr[i] === "--discard-prev" && i != 0) {
+  //     newArr.splice(i - 1, 2);
+  //   } else if (newArr[i] === "--discard-prev" && i === 0) {
+  //     return arr;
+  //   } else if (newArr[i] === "--double-next" && i != newArr.length - 1) {
+  //     newArr[i] = newArr[i + 1];
+  //   } else if (newArr[i] === "--double-next" && i === newArr.length - 1) {
+  //     return arr;
+  //   } else if (newArr[i] === "--double-prev" && i != 0) {
+  //     newArr[i] = newArr[i - 1];
+  //   } else if (newArr[i] === "--double-prev" && i === 0) {
+  //     return arr;
+  //   }
+  // }
+
+  // return newArr;
 };
