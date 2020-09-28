aa25346494defdf6662df47b45dd2fcd909f5df5
Evgene Popolamov <popolamov908@gmail.com>
2020-09-28 22:25:36 +0300

    feat: add solve transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,31 +1,34 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
-  throw new CustomError("Not implemented");
-  // remove line with error and write your code here
-  // if (!Array.isArray(arr)) {
-  //   throw new Error("Error");
-  // }
-  // const newArr = [...arr];
-  // for (let i = 0; i < newArr.length - 1; i++) {
-  //   if (newArr[i] === "--discard-next" && i != newArr.length - 1) {
-  //     newArr[i] = newArr[i + 1];
-  //   } else if (newArr[i] === "--discard-next" && i === newArr.length - 1) {
-  //     newArr[i] = "xxx";
-  //   } else if (newArr[i] === "--discard-prev" && i != 0) {
-  //     item[i - 1] = xxx;
-  //   } else if (newArr[i] === "--discard-prev" && i === 0) {
-  //     newArr[i] = "xxx";
-  //   } else if (newArr[i] === "--double-next" && i != newArr.length - 1) {
-  //     newArr[i] = newArr[i + 1];
-  //   } else if (newArr[i] === "--double-next" && i === newArr.length - 1) {
-  //     newArr[i] = "xxx";
-  //   } else if (newArr[i] === "--double-prev" && i != 0) {
-  //     item[i - 1] = xxx;
-  //   } else if (newArr[i] === "--double-prev" && i === 0) {
-  //     newArr[i] = "xxx";
-  //   }
-  // }
-  // newArr.filter((item) => item !== "xxx");
-  // return newArr;
+  if (!Array.isArray(arr)) {
+    throw new Error("Error");
+  }
+
+  const newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case "--discard-next":
+        i++;
+        break;
+
+      case "--discard-prev":
+        if (newArr.length && arr[i - 2] !== "--discard-next") newArr.pop();
+        break;
+
+      case "--double-next":
+        if (i < arr.length - 1) newArr.push(arr[i + 1]);
+        break;
+
+      case "--double-prev":
+        if (i && arr[i - 2] !== "--discard-next") newArr.push(arr[i - 1]);
+        break;
+
+      default:
+        newArr.push(arr[i]);
+    }
+  }
+
+  return newArr;
 };
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
+  encrypt(message, key) {
+    // if(message == undefined || key == undefined){
+    //   throw new Error('Error')
+    // }
+
+    // let newMessage = message.toUpperCase();
+    // let newKey = key.toUpperCase();
+    // let result = '';
+
+    // for(let i = 0; i <= newMessage.length; i++){
+
+    // }
+
+    throw new CustomError("Not implemented");
     // remove line with error and write your code here
-  }    
+  }
   decrypt() {
-    throw new CustomError('Not implemented');
+    throw new CustomError("Not implemented");
     // remove line with error and write your code here
   }
 }
